f1dbc2066add4c02b1c7e621d619733c96fda404
K. Gayathri <163125644+Gayathri1905@users.noreply.github.com>
2025-03-11 23:50:26 +0530

    Add files via upload

---
+++ greatest_number.dart
@@ -0,0 +1,18 @@
+import dart:io';
+void main(){
+stdout.write("Enter a:");
+int a-int.parse(stdin.readLineSync());
+stdout.write("Enter b:");
+int b=int.parse(stdin.readLineSync());
+stdout.write("Enter c:");
+int ceint.parse(stdin.readLineSync()!);
+if(a>b && a>c){
+print("$a is the greatest number");
+}
+else if(b>a 88 b>c){
+print("$b is the greatest number");
+}
+else{
+print("$c is the greatest number");
+}
+}
