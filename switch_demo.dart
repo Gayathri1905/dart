a17019d3555e95eea9e7ef7776ab24ed2c2d6b91
K. Gayathri <163125644+Gayathri1905@users.noreply.github.com>
2025-03-12 10:38:48 +0530

    Add files via upload

---
+++ switch_demo.dart
@@ -0,0 +1,12 @@
+void switchd1(String a) {
+ switch(a)
+ {
+  case 'red': print('the colour is red');
+  break;
+  case 'blue': print('the colour is blue');
+  break;
+  case 'green': print('the colour is green');
+  break;
+  case _:print('colour is not listed');
+ }
+}
