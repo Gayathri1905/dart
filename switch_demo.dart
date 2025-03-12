ae614abde305bf418cdf13b4241fc45dde38a038
K. Gayathri <163125644+Gayathri1905@users.noreply.github.com>
2025-03-12 10:59:12 +0530

    Add files via upload

--- switch_demo.dart
+++ switch_demo.dart
@@ -1,7 +1,9 @@
 void switchd1(String a) {
  switch(a)
  {
-  case 'red'||'blue': print('the colour is blue');
+  case 'red': 
+ 
+  case 'blue': print('the colour is blue');
  
   case 'green': print('the colour is green');
   
